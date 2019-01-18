3bc7cc8e3e08f46b7cbd63f96be0a109d63651d6
Gabriella Rose <gabriellarose@gabrielsmacbook.home>
2019-01-18 16:03:03 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -7,3 +7,24 @@ var cardFour = "king";
 
 console.log("User flipped " + cardTwo);
 console.log("User flipped " + cardThree);
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped queen");
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped king");
+
+if (cardsInPlay.length === 2) {
+	console.log("true dat")
+}
+
+if (cardsInPlay[3] === cardsInPlay[2]) {
+	alert("You found a match!")
+} else {
+	alert("Sorry, try again.")
+}
